70ec29303390baedeeaccf6188e5f9fb11603a2c
balwantsinghmnit <balwantsinghmnit@gmail.com>
2020-08-09 17:17:41 +0530

    Updates

---
+++ precache-manifest.3b31b1827700de8cdc30eefee2580a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc885e8d054e695c62716e3e1d142047",
+    "url": "/quizapp/index.html"
+  },
+  {
+    "revision": "e743f147a4dddb63ebd8",
+    "url": "/quizapp/static/css/main.5e1ad75b.chunk.css"
+  },
+  {
+    "revision": "c3d79780ac53ccea4e00",
+    "url": "/quizapp/static/js/2.7a0e306a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizapp/static/js/2.7a0e306a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e743f147a4dddb63ebd8",
+    "url": "/quizapp/static/js/main.2b80bda4.chunk.js"
+  },
+  {
+    "revision": "a6508cc90f14bba2473a",
+    "url": "/quizapp/static/js/runtime-main.ae6c255c.js"
+  }
+]);
